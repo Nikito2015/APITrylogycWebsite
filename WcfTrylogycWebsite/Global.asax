e431ff73a3754c5d03885d072f6723499de4e64f
Ricardo <ricardoivalle@hotmail.com>
2020-11-10 11:26:15 -0300

    deleted old projects

--- WcfTrylogycWebsite/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WcfTrylogycWebsite.Global" Language="C#" %>
--- WcfTrylogycWebsite/ServiceRequests/Interfaces/IUserAssociatesAndBalancesRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WcfTrylogycWebsite.ServiceRequests.Interfaces
-{
-    public interface IUserAssociatesAndBalancesRequest
-    {
-        int UserId { get; set; }
-
-        bool IsValid();
-    }
-}
